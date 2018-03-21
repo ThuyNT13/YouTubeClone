b8a3de28d92e1ffe22b6705a60f68fca971d129d
ThuyNT13 <ngo.thuy38@gmail.com>
2018-03-20 23:40:07 -0700

    Add imgURL and title to videoListItem.js

--- src/components/videoListItem.js
+++ src/components/videoListItem.js
@@ -3,16 +3,17 @@ import React from 'react';
 const VideoListItem = (props) => {
   const video = props.video;
   // console.log(video);
+  const imgURL = video.snippet.thumbnails.default.url;
 
     return (
       <li className="list-group-item">
         <div className="video-list media">
           <div className="media-left">
-            <img className="media-object"/>
+            <img className="media-object" src={imgURL}/>
           </div>
 
           <div className="media-body">
-            <div className="media-heading"></div>
+            <div className="media-heading">{video.snippet.title}</div>
           </div>
         </div>
       </li>
